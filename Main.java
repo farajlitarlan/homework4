df532745dea47a1c7546596fbd9e9c265c3a5f41
farajlitarlan <108183430+farajlitarlan@users.noreply.github.com>
2022-08-11 13:59:14 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package happy.Family;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pet pet = new Pet("dog",
+                "Cloud",
+                1, 20,
+                new String[]{"playing", "sleeping"});
+
+        Human mother = new Human("Anne", "Murphy", 1989);
+        Human father = new Human("Jack", "Murphy", 1984);
+
+        Human child = new Human("Belle","Murphy", 2000, 111, pet,
+                father,mother, new String[][]{{"sunday", "walking"}}) ;
+
+        child.greetPet() ;
+        child.describePet();
+        pet.respond();
+        pet.eat();
+
+        System.out.println(child);
+    }
+}
